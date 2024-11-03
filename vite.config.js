d254924511332fe1fecc757fc3512dd3582ceb88
o.ganzienko <ganzienko@gmail.com>
2024-11-04 01:22:49 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig({
   plugins: [
+    injectHTML(),
+    FullReload(['./src/**/**.html']),
+    SortCss({ sort: 'mobile-first' }),
     viteStaticCopy({
       targets: [
         {
@@ -10,10 +16,35 @@ export default defineConfig({
           dest: ''
         }
       ]
-    })
+    }),
   ],
+  root: 'src',
   build: {
     base: '/goit-advancedjs-hw-01/',
-    outDir: 'dist',
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: './src/index.html',
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        entryFileNames: chunkInfo => {
+          if (chunkInfo.name === 'commonHelpers') {
+            return 'commonHelpers.js';
+          }
+          return '[name].js';
+        },
+        assetFileNames: assetInfo => {
+          if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+            return '[name].[ext]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
   },
 });
