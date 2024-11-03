ebefe7d664284edf6b3d86c5aba0caefda1dee39
o.ganzienko <ganzienko@gmail.com>
2024-11-04 00:32:05 +0200

    update-8

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,15 +1,23 @@
 function openModal(imgElement) {
-    var modal = document.getElementById("modal");
-    var modalImg = document.getElementById("modal-image");
-    var captionText = document.getElementById("caption");
-  
-    modal.style.display = "block";
-    modalImg.src = imgElement.getAttribute("data-large");
-    captionText.innerHTML = imgElement.alt;
+  var modal = document.getElementById("modal");
+  var modalImg = document.getElementById("modal-image");
+  var captionText = document.getElementById("caption");
+
+  modal.style.display = "block";
+  modalImg.src = imgElement.getAttribute("data-large");
+  captionText.innerHTML = imgElement.alt;
+}
+
+function closeModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
+
+document.querySelector(".close").addEventListener("click", closeModal);
+
+window.addEventListener("click", function(event) {
+  var modal = document.getElementById("modal");
+  if (event.target === modal) {
+    closeModal();
   }
-  
-  function closeModal() {
-    var modal = document.getElementById("modal");
-    modal.style.display = "none";
-  }
-  
+});
