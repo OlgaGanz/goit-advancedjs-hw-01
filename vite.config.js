c4b5c2c2a4d40740e46048973ddee0cc23d40d6e
o.ganzienko <ganzienko@gmail.com>
2024-11-04 02:27:40 +0200

    4

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,48 @@
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig({
-  plugins: [
-    injectHTML(),
-    FullReload(['./src/**/**.html']),
-    SortCss({ sort: 'mobile-first' }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/images',
-          dest: ''
-        }
-      ]
-    }),
-  ],
-  root: 'src',
-  build: {
-    base: '/goit-advancedjs-hw-01/',
-    outDir: '../dist',
-    emptyOutDir: true,
-    sourcemap: true,
-    rollupOptions: {
-      input: './src/index.html',
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-        entryFileNames: chunkInfo => {
-          if (chunkInfo.name === 'commonHelpers') {
-            return 'commonHelpers.js';
-          }
-          return '[name].js';
-        },
-        assetFileNames: assetInfo => {
-          if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-            return '[name].[ext]';
-          }
-          return 'assets/[name]-[hash][extname]';
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
         },
       },
+      outDir: '../dist',
+      emptyOutDir: true,
     },
-  },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
+  };
 });
